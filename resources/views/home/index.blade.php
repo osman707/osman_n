5ed737aaed688cc128e98b1ac961a3535c14d614
osman707 <nazarovosman2000@gmail.com>
2024-07-24 01:55:17 +0500

    1.6.0

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,1 +1,4 @@
-<?php
+@extends('layouts.app')
+@section('title')
+    Home
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title') - AtaCar</title>
+    <title>@yield('title') - Career</title>
 <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
 <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 <script type="text/javascript" src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
